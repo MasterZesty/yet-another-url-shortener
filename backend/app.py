bedca0ef207016e4ce8c513c2e25e48dac185188
MasterZasty <kgn272000@gmail.com>
2024-03-16 01:48:39 +0530

    backend dockerfile

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,27 @@ from db import DB
 from models import ShortenedURL
 from sqlalchemy.exc import IntegrityError
 from flask_cors import CORS
-
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)
+
+# swagger stuff
+
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
+
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+
 
 DB_URI = "sqlite:///url_shortner.db"
 db = DB(DB_URI)
@@ -95,3 +112,7 @@ def alias_available(custom_alias: str):
     }
 
     return jsonify(is_alias_available), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
